73d7eeacec1c33b45f2a4f02b23f3037d18e94d0
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-11-15 10:41:07 +0200

    get_philosophers.js

--- get_philosophers.js
+++ get_philosophers.js
@@ -20,9 +20,17 @@ document.addEventListener('DOMContentLoaded', function() {
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
       const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-        console.log(jsonData);
-      outputDiv.innerText = JSON.stringify(jsonData, null, 2);
+     for(el of content){
+   data+=`
+   <tr>
+    <td>${el.name}</td>
+    <td>${el.date}</td>
+    <td>${el.age}</td>
+   </tr>
+   `
+}
+output.innerHTML=data
     };
   
-    xhr.send();
+    
   }
