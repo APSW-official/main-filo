fd4cc4686b46e0c2edcbb4751a49aa86e2f6901e
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-11-16 00:40:41 +0200

    get_philosophers.js

--- get_philosophers.js
+++ get_philosophers.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let data1="";
     data1+=`<table style="border-collapse: collapse;">`
         let aux=jsonData[0][1];
-        data1+=`<tr>
+        data1+=`<tr style="border-bottom 1xp white;">
         <td>${aux.toUpperCase()}</td>
         </tr>
         `
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
 for(el of jsonData){
      if(el[1]!==aux){
         aux=el[1];
-        data1+=`<tr>
+        data1+=`<tr style="border-bottom 1xp white;border-top:1px white;">
         <td>${aux.toUpperCase()}</td>
         </tr>
         `
