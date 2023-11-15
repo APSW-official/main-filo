93b57c315eb808adb256b195e32a97c69f766903
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-11-15 10:13:28 +0200

    Add files via upload

---
+++ get_philosophers.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    processFile();
+  });
+
+
+  function processFile() {
+    const filePath = 'C:\Users\Serban\all\Serban.doc\Scoala\Socio-umane\site-filo\text.xlsx'; // Update with your actual file path
+    const outputDiv = document.getElementById('output');
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.responseType = 'arraybuffer';
+  
+    xhr.onload = function () {
+      const arrayBuffer = xhr.response;
+      const data = new Uint8Array(arrayBuffer);
+      const workbook = XLSX.read(data, { type: 'array' });
+  
+      // Process the workbook, for example, display the content in the outputDiv
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+  
+      outputDiv.innerText = JSON.stringify(jsonData, null, 2);
+    };
+  
+    xhr.send();
+  }
