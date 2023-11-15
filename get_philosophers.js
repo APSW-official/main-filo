692aeda0eac9ffa24fda8f7f6fa7e921e2da07a2
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-11-15 11:37:14 +0200

    get_philosophers.js

--- get_philosophers.js
+++ get_philosophers.js
@@ -22,13 +22,12 @@ document.addEventListener('DOMContentLoaded', function() {
       const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
         console.log(jsonData);
     let data1="";
-     for(el of jsonData){
+for(el of jsonData){
+         
    data1+=`
    <tr>
-                <td>${el[0]}</td>
-                <td>${el[1]}</td>
-                <td>${el[2]}</td>
-            </tr>
+       <td>${el[0]}</td>
+    </tr>
    `
 }
 outputDiv.innerHTML=data1
