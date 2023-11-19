00da40475e6857d8d899b630c51e0a8f3cb7187f
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-11-19 23:46:30 +0200

    get_philosophers.js

---
+++ get_philosophers.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    processFile();
+  });
+
+
+  function processFile() {
+    const filePath = 'filozofi.xlsx'; // Update with your actual file path
+    const outputDiv = document.getElementById('output');
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.responseType = 'arraybuffer';
+  
+    xhr.onload = function () {
+      const arrayBuffer = xhr.response;
+      const data = new Uint8Array(arrayBuffer);
+      const workbook = XLSX.read(data, { type: 'array' });
+  
+      // Process the workbook, for example, display the content in the outputDiv
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        console.log(jsonData);
+    let data1="";
+    
+
+        
+for(el of jsonData){
+
+   data1+=`
+   <tr>
+       <td   >${el[0]}</td>
+    </tr>
+   `
+}
+outputDiv.innerHTML=data1
+    };
+  
+     xhr.send();
+  }
