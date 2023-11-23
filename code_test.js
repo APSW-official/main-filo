2f7b1c26e8b50a556ddaf5c33b3fc57e128ba955
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-11-23 09:24:07 +0200

    Add files via upload

---
+++ code_test.js
@@ -0,0 +1,65 @@
+let dif, ans, hints,ok=0;
+
+function start() {
+
+    if (!(document.getElementById("toggleSwitch").checked)) {
+        // If the toggle is on, check for all variables to have values
+        if (dif !== undefined && ans !== undefined && hints !== undefined) {
+            console.log("All three variables have values");
+            console.log("Dif:", dif);
+            console.log("Ans:", ans);
+            console.log("Hints:", hints);ok=1;
+        } else {
+            console.log("Not all required variables have values");
+            alert("Please provide values for all inputs.");
+        }
+    } else {
+        // If the toggle is off, check if ans and dif have values
+        if (dif !== undefined && ans !== undefined) {
+            console.log("Only Dif and Ans have values");
+            console.log("Dif:", dif);
+            console.log("Ans:", ans);
+            ok=1;
+        } else {
+            console.log("Not all required variables have values");
+            alert("Please provide values for all inputs.");
+        }
+    }
+
+
+if(ok){
+    let startDiv=document.getElementById("startDiv");
+    startDiv.style.display="none";
+    
+    display_quest();
+}
+
+
+
+}
+
+
+function get_dif(x) {
+    dif = x;
+}
+
+function get_ans(x) {
+    ans = x;
+}
+
+function get_hints(x) {
+    hints = x;
+}
+
+
+function display_quest() {
+    let testDiv = document.getElementById("testDiv");
+    testDiv.style.display = "flex";
+    
+    let data = '';
+    data += `<div style="display: flex; justify-content: center; align-items: center; height: 100%;">`;
+    data += `<center><h3>Intrebare</h3></center>`;
+    data += `</div>`;
+    
+    testDiv.innerHTML = data;
+}
