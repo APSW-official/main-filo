4fedb91bb4a60f4333e1baf76c61c0f3fb6eced9
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-12-07 01:22:56 +0200

    Update code_test.js

--- code_test.js
+++ code_test.js
@@ -7,46 +7,7 @@ let RAns,fil, filC;
 
 const jsonData;
 
-function start() {
 
-    if (!(document.getElementById("toggleSwitch").checked)) {
-        // If the toggle is on, check for all variables to have values
-        if (dif !== undefined && ans !== undefined && hints !== undefined) {
-            console.log("All three variables have values");
-            console.log("Dif:", dif);
-            console.log("Ans:", ans);
-            console.log("Hints:", hints);ok=1;
-        } else {
-            console.log("Not all required variables have values");
-            alert("Please provide values for all inputs.");
-        }
-    } else {
-        // If the toggle is off, check if ans and dif have values
-        if (dif !== undefined && ans !== undefined) {
-            console.log("Only Dif and Ans have values");
-            console.log("Dif:", dif);
-            console.log("Ans:", ans);
-            ok=1;
-        } else {
-            console.log("Not all required variables have values");
-            alert("Please provide values for all inputs.");
-        }
-    }
-
-
-if(ok){
-    let startDiv=document.getElementById("startDiv");
-    startDiv.style.display="none";
-    
-    //processFile();
-
-    display_quest();
-    
-}
-
-
-
-}
 
 
 function get_dif(x) {
@@ -66,6 +27,45 @@ function get_selAns(x){
     console.log(x===RAns);
 }
 
+function processFile() {
+    const filePath = 'filozofi.xlsx'; // Update with your actual file path
+    const outputDiv = document.getElementById('output');
+    let data1 = ""; // Declare data1 here to make it accessible within the entire function
+    let prevLet;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function () {
+        const arrayBuffer = xhr.response;
+        const data = new Uint8Array(arrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        // Process the workbook, for example, display the content in the outputDiv
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        console.log(jsonData);
+}
+
+
+
+function create_quest(){
+    
+    fil=get_rand(25);
+    filC=get_rand(20);
+    RAns=get_rand(ans);
+    otherAns[RAns]=fil;
+    console.log(RAns);
+    let i=1;
+    while(i<ans){
+        let aux1=get_rand(ans),aux2=get_rand(25);
+        console.log(i+" "+aux1);
+        if(aux1!==RAns&&typeof otherAns[aux1] === 'undefined'&&aux2!==fil){
+            otherAns[aux1]=aux2;i++;
+        }
+    }
+}
 
 function display_quest() {
     create_quest();
@@ -115,44 +115,52 @@ function get_rand(x){
 }
 
 
-function create_quest(){
-    
-    fil=get_rand(25);
-    filC=get_rand(20);
-    RAns=get_rand(ans);
-    otherAns[RAns]=fil;
-    console.log(RAns);
-    let i=1;
-    while(i<ans){
-        let aux1=get_rand(ans),aux2=get_rand(25);
-        console.log(i+" "+aux1);
-        if(aux1!==RAns&&typeof otherAns[aux1] === 'undefined'&&aux2!==fil){
-            otherAns[aux1]=aux2;i++;
+
+
+
+
+
+
+
+
+function start() {
+
+    if (!(document.getElementById("toggleSwitch").checked)) {
+        // If the toggle is on, check for all variables to have values
+        if (dif !== undefined && ans !== undefined && hints !== undefined) {
+            console.log("All three variables have values");
+            console.log("Dif:", dif);
+            console.log("Ans:", ans);
+            console.log("Hints:", hints);ok=1;
+        } else {
+            console.log("Not all required variables have values");
+            alert("Please provide values for all inputs.");
+        }
+    } else {
+        // If the toggle is off, check if ans and dif have values
+        if (dif !== undefined && ans !== undefined) {
+            console.log("Only Dif and Ans have values");
+            console.log("Dif:", dif);
+            console.log("Ans:", ans);
+            ok=1;
+        } else {
+            console.log("Not all required variables have values");
+            alert("Please provide values for all inputs.");
         }
     }
+
+
+if(ok){
+    let startDiv=document.getElementById("startDiv");
+    startDiv.style.display="none";
+    
+    //processFile();
+
+    display_quest();
+    
 }
 
 
 
-
-
-function processFile() {
-    const filePath = 'filozofi.xlsx'; // Update with your actual file path
-    const outputDiv = document.getElementById('output');
-    let data1 = ""; // Declare data1 here to make it accessible within the entire function
-    let prevLet;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', filePath, true);
-    xhr.responseType = 'arraybuffer';
-
-    xhr.onload = function () {
-        const arrayBuffer = xhr.response;
-        const data = new Uint8Array(arrayBuffer);
-        const workbook = XLSX.read(data, { type: 'array' });
-
-        // Process the workbook, for example, display the content in the outputDiv
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-        console.log(jsonData);
 }
+
