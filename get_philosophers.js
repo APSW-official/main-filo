0c58f0d8915fdf7ceb5e3b9adf649425e91c9c9e
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-12-07 08:57:39 +0200

    Update get_philosophers.js

--- get_philosophers.js
+++ get_philosophers.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function processFile() {
     const filePath = 'filozofi.xlsx'; // Update with your actual file path
     const outputDiv = document.getElementById('output');
-    let data1 = ""; // Declare data1 here to make it accessible within the entire function
+    let data1 = "";
     let prevLet;
     const xhr = new XMLHttpRequest();
     xhr.open('GET', filePath, true);
@@ -16,38 +16,40 @@ function processFile() {
         const data = new Uint8Array(arrayBuffer);
         const workbook = XLSX.read(data, { type: 'array' });
 
-        // Process the workbook, for example, display the content in the outputDiv
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
         const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
         console.log(jsonData);
-        
-        prevLet=jsonData[0][1];
-data1 += `
-    <tr class="modified-prevLet-bot" id="${prevLet.toUpperCase()}">
-        <td >${prevLet.toUpperCase()}</td>
-    </tr>
-`;
 
-for (el of jsonData) {
-    if (el[1] != prevLet) {
-        prevLet = el[1];
+        // Call processFile2 with the necessary data
+        processFile2(jsonData);
+
+        prevLet = jsonData[0][1];
         data1 += `
-             <tr class="modified-prevLet" id="${prevLet.toUpperCase()}">
-                <td>${prevLet.toUpperCase()}</td>
+            <tr class="modified-prevLet-bot" id="${prevLet.toUpperCase()}">
+                <td >${prevLet.toUpperCase()}</td>
             </tr>
         `;
-    }
-    data1 += `
-        <tr>
-            <td>${el[0]}</td>
-        </tr>
-    `;
-}
 
-outputDiv.innerHTML = data1;
+        for (el of jsonData) {
+            if (el[1] != prevLet) {
+                prevLet = el[1];
+                data1 += `
+                    <tr class="modified-prevLet" id="${prevLet.toUpperCase()}">
+                        <td>${prevLet.toUpperCase()}</td>
+                    </tr>
+                `;
+            }
+            data1 += `
+                <tr>
+                    <td>${el[0]}</td>
+                </tr>
+            `;
+        }
 
+        outputDiv.innerHTML = data1;
     };
-processFile2(jsonData);
+
     xhr.send();
 }
+
