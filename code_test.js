33874576a77abd0873c860ed5636e9144130520e
APSW-official <117712437+APSW-official@users.noreply.github.com>
2023-12-09 22:21:38 +0200

    Update code_test.js

--- code_test.js
+++ code_test.js
@@ -131,9 +131,9 @@ function display_quest() {
         for (let col = 1; col <= 2; col++) {
             const index = (row - 1) * 2 + col;
             console.log(index);
-            if (index <= ans) {
+            
                 table_ans += `<td><button onclick="get_selAns(${index})">${otherAns[index]}</button></td>`;
-            }
+            
         }
 
         table_ans += '</tr>';
