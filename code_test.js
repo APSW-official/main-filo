b4d44203ed531676950e7a4e3ff797d4c00e5365
APSW-official <117712437+APSW-official@users.noreply.github.com>
2024-01-15 08:32:31 +0200

    Update code_test.js

--- code_test.js
+++ code_test.js
@@ -5,7 +5,11 @@ let RAns, fil, filC;
 let p=1,y=0;
 let testDiv;
 
-
+funtion location(){
+    let div=document.getElementById("startDiv");
+    console.log(div.style.display);
+    
+}
 function start() {
     if (!(document.getElementById("toggleSwitch").checked)) {
         if (dif !== undefined && ans !== undefined && hints !== undefined) {
