fc26e08328f9ab7dd6e8ab528736ad44d9e60eb6
APSW-official <117712437+APSW-official@users.noreply.github.com>
2024-01-24 14:58:18 +0200

    Update get_philosophers.js

--- get_philosophers.js
+++ get_philosophers.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     processFile();
 });
 
@@ -51,5 +51,81 @@ function processFile() {
     };
 
     xhr.send();
+}*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    processFile();
+    setupButtonEventListeners();
+});
+
+function processFile() {
+    const filePath = 'filozofi.xlsx'; // Update with your actual file path
+    const outputDiv = document.getElementById('output');
+    let data1 = "";
+    let prevLet;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function () {
+        const arrayBuffer = xhr.response;
+        const data = new Uint8Array(arrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        console.log(jsonData);
+
+        prevLet = jsonData[0][1];
+        data1 += `
+            <tr class="modified-prevLet-bot" id="${prevLet.toUpperCase()}">
+                <td >${prevLet.toUpperCase()}</td>
+            </tr>
+        `;
+
+        for (el of jsonData) {
+            if (el[1] != prevLet) {
+                prevLet = el[1];
+                data1 += `
+                    <tr class="modified-prevLet" id="${prevLet.toUpperCase()}">
+                        <td>${prevLet.toUpperCase()}</td>
+                    </tr>
+                `;
+            }
+            data1 += `
+                <tr>
+                    <td>${el[0]}</td>
+                </tr>
+            `;
+        }
+
+        outputDiv.innerHTML = data1;
+    };
+
+    xhr.send();
 }
+
+function setupButtonEventListeners() {
+    const links = document.querySelectorAll('.sticky-menu button');
+
+    links.forEach(link => {
+        link.addEventListener('click', function () {
+            const targetId = this.innerHTML.toUpperCase();
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                highlightAndFade(targetElement);
+            }
+        });
+    });
+
+    function highlightAndFade(element) {
+        element.classList.add('highlight');
+        setTimeout(() => {
+            element.classList.remove('highlight');
+        }, 2000);
+    }
+}
+
 
