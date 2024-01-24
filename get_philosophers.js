b6faba384cc439928ec94d3fd1ea6ea9b9e2e80d
APSW-official <117712437+APSW-official@users.noreply.github.com>
2024-01-24 15:58:10 +0200

    Update get_philosophers.js

--- get_philosophers.js
+++ get_philosophers.js
@@ -58,63 +58,6 @@ document.addEventListener('DOMContentLoaded', function () {
     setupButtonEventListeners();
 });
 
-function processFile() {
-    const filePath = 'filozofi.xlsx'; // Update with your actual file path
-    const outputDiv = document.getElementById('output');
-    let data1 = "";
-    let prevLet;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', filePath, true);
-    xhr.responseType = 'arraybuffer';
-
-    xhr.onload = function () {
-        const arrayBuffer = xhr.response;
-        const data = new Uint8Array(arrayBuffer);
-        const workbook = XLSX.read(data, { type: 'array' });
-
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-        console.log(jsonData);
-
-        prevLet = jsonData[0][1];
-        data1 += `
-            <tr class="modified-prevLet-bot" id="${prevLet.toUpperCase()}">
-                <td >${prevLet.toUpperCase()}</td>
-            </tr>
-        `;
-
-   // ...
-
-for (let i = 0; i < jsonData.length; i++) {
-    const el = jsonData[i];
-
-    if (el[1] != prevLet) {
-        prevLet = el[1];
-        data1 += `
-            <tr class="modified-prevLet" id="${prevLet.toUpperCase()}">
-                <td>${prevLet.toUpperCase()}</td>
-            </tr>
-        `;
-    }
-
-    data1 += `
-        <tr>
-            <td><button class="custom-button" id="${i}">${el[0]}</button></td>
-        </tr>
-    `;
-}
-
-// ...
-
-
-
-        outputDiv.innerHTML = data1;
-    };
-
-    xhr.send();
-}
-
 function setupButtonEventListeners() {
     const links = document.querySelectorAll('.sticky-menu button');
 
@@ -136,5 +79,63 @@ function setupButtonEventListeners() {
         }, 2000);
     }
 }
+
+
+
+function processFile() {
+    const filePath = 'filozofi.xlsx'; // Update with your actual file path
+    const outputDiv = document.getElementById('output');
+    let data1 = "";
+    let prevLet;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function () {
+        const arrayBuffer = xhr.response;
+        const data = new Uint8Array(arrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        fil_page(jsonData);
+
+        prevLet = jsonData[0][1];
+        data1 += `
+            <tr class="modified-prevLet-bot" id="${prevLet.toUpperCase()}">
+                <td >${prevLet.toUpperCase()}</td>
+            </tr>
+        `;
+        
+        for (let i = 0; i < jsonData.length; i++) {
+            const el = jsonData[i];
+            if (el[1] != prevLet) {
+                prevLet = el[1];
+                data1 += `
+                    <tr class="modified-prevLet" id="${prevLet.toUpperCase()}">
+                        <td>${prevLet.toUpperCase()}</td>
+                    </tr>
+                `;
+            }
+        
+            data1 += `
+                <tr>
+                    <td><button class="custom-button" onclick=fil_page(${i})>${el[0]}</button></td>
+                </tr>
+            `;
+        }
+        
+        outputDiv.innerHTML = data1;
+    };
+
+    xhr.send();
+}
+
+function fil_page(filId){
+    console.log(filId);
+}
+
+
 
 
